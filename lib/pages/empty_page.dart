c86bda54004903b1573a771f2a112ba4a2ab163b
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-06-22 23:37:33 +0700

    add: empty page for case no data in view

---
+++ lib/pages/empty_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Image.asset('assets/images/empty_box.jpeg'),
+        Text(
+          description,
+          style: const TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+}
