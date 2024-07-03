b80a08dd97626957a6ef14157877c8e00d1d4b32
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-07-03 11:33:54 +0700

    add: badge icon for cart

--- lib/pages/bottom_bar_page.dart
+++ lib/pages/bottom_bar_page.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:keyboard_shop/pages/cart_page.dart';
@@ -50,23 +51,37 @@ class _MyBottomBarPage extends State<MyBottomBarPage> {
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedIndex,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
             icon: Icon(IconlyLight.home),
             activeIcon: Icon(IconlyBold.home),
             label: 'Home',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(IconlyLight.category),
             activeIcon: Icon(IconlyBold.category),
             label: 'Categories',
           ),
           BottomNavigationBarItem(
-            icon: Icon(IconlyLight.buy),
-            activeIcon: Icon(IconlyBold.buy),
+            icon: badges.Badge(
+              badgeContent: Text(
+                '0',
+                style: TextStyle(color: Theme.of(context).colorScheme.background),
+              ),
+              badgeStyle: badges.BadgeStyle(badgeColor: Theme.of(context).colorScheme.inversePrimary),
+              child: const Icon(IconlyLight.buy),
+            ),
+            activeIcon: badges.Badge(
+              badgeContent: Text(
+                '0',
+                style: TextStyle(color: Theme.of(context).colorScheme.background),
+              ),
+              badgeStyle: badges.BadgeStyle(badgeColor: Theme.of(context).colorScheme.inversePrimary),
+              child: const Icon(IconlyBold.buy),
+            ),
             label: 'Cart',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(IconlyLight.user2),
             activeIcon: Icon(IconlyBold.user2),
             label: 'User',
