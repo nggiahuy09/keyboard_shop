ae9d5d091bd4d6da72ecdb12f3d015b14f00fc96
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-07-30 16:21:07 +0700

    add: utilities file for some methods

---
+++ lib/services/utilities.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class Utils {
+  BuildContext context;
+  Utils(this.context);
+
+  Size get screenSize => MediaQuery.of(context).size;
+  Color get primaryColor => Theme.of(context).colorScheme.inversePrimary;
+}
