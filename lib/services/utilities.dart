d43d6cf831d47eacbe7cbceefd5fa7c998af1998
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-08-08 14:07:58 +0700

    add: custom show snack bar function

--- lib/services/utilities.dart
+++ lib/services/utilities.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:keyboard_shop/consts/firebase_const.dart';
 
 class Utils {
   BuildContext context;
@@ -7,4 +8,18 @@ class Utils {
 
   Size get screenSize => MediaQuery.of(context).size;
   Color get primaryColor => Theme.of(context).colorScheme.inversePrimary;
+
+  static void showSnackBar(BuildContext context, {required String msg}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          msg,
+        ),
+      ),
+    );
+  }
+
+  static bool checkHasLogin() {
+    return authInstance.currentUser?.uid != null ? true : false;
+  }
 }
