245302502efc75b31df08b4a5eaeb000db1751dc
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-08-17 16:23:08 +0700

    add: create separate fetch screen for fetching data from firebase services

---
+++ lib/pages/fetch_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_shop/pages/bottom_bar_page.dart';
+import 'package:keyboard_shop/providers/products_provider.dart';
+import 'package:provider/provider.dart';
+
+class FetchScreen extends StatefulWidget {
+  const FetchScreen({super.key});
+
+  @override
+  State<FetchScreen> createState() => _FetchScreenState();
+}
+
+class _FetchScreenState extends State<FetchScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(microseconds: 8)).then(
+      (_) async {
+        final productsProvider = Provider.of<ProductsProvider>(context, listen: false);
+        await productsProvider.fetchProducts();
+
+        if (mounted) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) {
+                return const MyBottomBarPage();
+              },
+            ),
+          );
+        }
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background.withOpacity(0.8),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons/app_icon.png',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 24),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
